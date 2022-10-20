c3a877157a389c37afc1c2344a6d7920456afc12
jielyu <jiejielyu@outlook.com>
2022-10-20 21:41:07 +0800

    feat: 增加2015和2016，其中2016解析html存在问题导致结果不完整，2015可能存在部分重复

--- d0015_cvpaper_spider/src/cvpr2015_parser.py
+++ d0015_cvpaper_spider/src/cvpr2015_parser.py
@@ -11,9 +11,82 @@ class CVPRParser(CVConfParser):
     def __init__(self) -> None:
         super().__init__("cvpr", 2015)
 
+    def parse_pages(self):
+        time_tag = self.dom.select(".cvprparagraphheader")
+        all_papers = []
+        for tag in time_tag:
+            # 从时间中解析tag
+            time_str = utils.strip_html_text(tag.get_text()).split(",")[0]
+            day = time_str.split()[-1]
+            sess_tag = "6" + "%0*d" % (2, int(day))
+            # print(sess_tag)
+            # 解析table
+            table_tag = tag.find_next_sibling("table")
+            heads = table_tag.select("th")
+            if len(heads) == 1:
+                # 去掉speaker
+                if "speaker" in heads[0].get_text().lower():
+                    continue
+                # 解析poster
+                rows = table_tag.select("tr")
+                for row in rows[3:]:
+                    cols = row.select("td")
+                    poster_id = utils.strip_html_text(cols[0].get_text())
+                    title_tag = cols[1].select("b")[0]
+                    title = utils.strip_html_text(title_tag.get_text())
+                    authors = utils.strip_html_text(col.get_text().split("]")[-1])
+                    all_papers.append(
+                        {
+                            "type": "posters",
+                            "sess_title": "",
+                            "poster_id": sess_tag + "-" + poster_id,
+                            "paper_id": "",
+                            "title": title,
+                            "authors": [
+                                " ".join(author.strip().split())
+                                for author in authors.split(",")
+                            ],
+                        }
+                    )
+            elif len(heads) == 2:
+                # 解析oral
+                rows = table_tag.select("tr")
+                idx = 0
+                for row in rows[2:]:
+                    cols = row.select("td")
+                    for col in cols:
+                        idx += 1
+                        # print(col)
+                        title_tag = col.select("b")
+                        if len(title_tag) == 0:
+                            continue
+                        title = utils.strip_html_text(title_tag[0].get_text())
+                        authors = utils.strip_html_text(col.get_text().split("]")[-1])
+                        all_papers.append(
+                            {
+                                "type": "orals",
+                                "sess_title": "",
+                                "poster_id": sess_tag + "-" + str(idx),
+                                "paper_id": "",
+                                "title": title,
+                                "authors": [
+                                    " ".join(author.strip().split())
+                                    for author in authors.split(",")
+                                ],
+                            }
+                        )
+            else:
+                raise ValueError("not expect table")
+        return all_papers
+
 
 def main():
     cvpr = CVPRParser()
+    all_papers = cvpr.parse_pages()
+    print("number of papers: {}".format(len(all_papers)))
+    # 写入到文件
+    with open("output/cvpr2015.json", "w") as fid:
+        json.dump(all_papers, fid, indent=4, separators=(",", ": "))
 
 
 if __name__ == "__main__":
--- d0015_cvpaper_spider/src/cvpr2016_parser.py
+++ d0015_cvpaper_spider/src/cvpr2016_parser.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+from ctypes import util
 import json
 
 import utils
@@ -11,9 +12,76 @@ class CVPRParser(CVConfParser):
     def __init__(self) -> None:
         super().__init__("cvpr", 2016)
 
+    def _parse_session(self, tag, paper_type):
+        papers = []
+        sess_title = ""
+        sess_tag = "6"
+        daytime_tag = ""
+        if "oral" == paper_type or "poster" == paper_type:
+            paper_type = paper_type + "s"
+        while tag.name != "hr":  # tag != None and
+            if tag.name == "h4":
+                sess_title = utils.strip_html_text(tag.get_text())
+            elif tag.name == "h5":
+                daytime_tag = utils.strip_html_text(tag.get_text())
+                items = daytime_tag.split(",")
+                date = items[1].strip().split()[-1]
+                daytime = "a" if "AM" in items[2] else "b"
+                sess_tag = date[0:2] + "-" + daytime
+            elif tag.name == "ul":
+                title_tag = tag.select("strong")[0]
+                author_tag = tag.select("p")[0]
+                poster_id = tag.get_text().strip().split()[0].strip()
+                # print(title_tag)
+                title = utils.strip_html_text(title_tag.get_text())
+                authors = utils.strip_html_text(author_tag.get_text())
+                papers.append(
+                    {
+                        "type": paper_type,
+                        "sess_title": sess_title,
+                        "poster_id": sess_tag + "-" + poster_id,
+                        "paper_id": "",
+                        "title": title,
+                        "authors": [
+                            " ".join(author.strip().split())
+                            for author in authors.split(",")
+                        ],
+                    }
+                )
+                # print("ul", poster_id)
+            # print(tag.name)
+            nexttag = tag.find_next_sibling()
+            if nexttag is None:
+                print(daytime_tag, "poster_id:", poster_id)
+                print("not found next")
+                break
+            tag = nexttag
+        return papers
+
+    def parse_pages(self):
+        """bs4似乎解析html存在问题"""
+        all_headers = self.dom.select("h3.programheader")
+        print(len(all_headers))
+        all_papers = []
+        for header in all_headers:
+            h3 = header
+            paper_type = utils.strip_html_text(h3.get_text()).split("SESSION")[0]
+            paper_type = paper_type.strip().lower()
+            tag = header.find_next_sibling()
+            papers = self._parse_session(tag, paper_type)
+            print("number:", len(papers))
+            # for paper in papers:
+            all_papers.extend(papers)
+        return all_papers
+
 
 def main():
     cvpr = CVPRParser()
+    all_papers = cvpr.parse_pages()
+    print("number of papers: {}".format(len(all_papers)))
+    # 写入到文件
+    with open("output/cvpr2016.json", "w") as fid:
+        json.dump(all_papers, fid, indent=4, separators=(",", ": "))
 
 
 if __name__ == "__main__":
